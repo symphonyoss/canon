11e16cd2e1eff1c1f5bd979277166eeead7c4d5f
bruceskingle <bruce.skingle@symphony.com>
2018-11-16 14:21:15 +0000

    Prepare for release

---
+++ canon-runtime-java/src/main/java/org/symphonyoss/s2/canon/runtime/EntityPubSubMessage.java
@@ -0,0 +1,50 @@
+/*
+ *
+ *
+ * Copyright 2018 Symphony Communication Services, LLC.
+ *
+ * Licensed to The Symphony Software Foundation (SSF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.symphonyoss.s2.canon.runtime;
+
+import org.symphonyoss.s2.fugue.core.trace.ITraceContext;
+import org.symphonyoss.s2.fugue.pubsub.PubSubMessage;
+
+/**
+ * A PubSubMessage for a Canon entity payload.
+ * 
+ * Sets the payload type attribute.
+ * 
+ * @author Bruce Skingle
+ *
+ */
+public class EntityPubSubMessage extends PubSubMessage
+{
+  /**
+   * Constructor.
+   * 
+   * @param payload       The message payload.
+   * @param traceContext  A Trace Context.
+   */
+  public EntityPubSubMessage(IEntity payload, ITraceContext traceContext)
+  {
+    super(payload.toString(), traceContext, payload.getCanonType());
+  }
+
+}
